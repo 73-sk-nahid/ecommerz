73c1265dd26ec9c3f14ca84e10ae4aaff8a47b57
Sk. Nahid <nahidsheikh2001@gmail.com>
2025-01-26 00:07:38 +0600

    modify routing

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -7,6 +7,7 @@ import 'package:ecommerz/features/auth/ui/screens/splash_screen.dart';
 import 'package:ecommerz/features/category/ui/screens/category_list_screen.dart';
 import 'package:ecommerz/features/common/ui/screens/main_bottom_nav_screen.dart';
 import 'package:ecommerz/features/home/ui/screens/home_screen.dart';
+import 'package:ecommerz/features/product/ui/screens/product_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -21,14 +22,29 @@ class CraftyBay extends StatelessWidget {
       theme: AppThemeData.lightThemeData,
       darkTheme: AppThemeData.darkThemeData,
       initialBinding: ControllerBinder(),
-      routes: {
-        "/" : (context) => const SplashScreen(),
-        EmailVerifyScreen.name : (context) => const EmailVerifyScreen(),
-        OTPVerifyScreen.name : (context) => const OTPVerifyScreen(),
-        CompleteProfileScreen.name : (context) => const CompleteProfileScreen(),
-        HomeScreen.name : (context) => const HomeScreen(),
-        MainBottomNavScreen.name : (context) => const MainBottomNavScreen(),
-        CategoryListScreen.name : (context) => const CategoryListScreen(),
+      onGenerateRoute: (RouteSettings settings) {
+        late Widget widget;
+        if (settings.name == SplashScreen.name) {
+          widget = const SplashScreen();
+        } else if (settings.name == EmailVerifyScreen.name) {
+          widget = const EmailVerifyScreen();
+        } else if (settings.name == OTPVerifyScreen.name) {
+          widget = const OTPVerifyScreen();
+        } else if (settings.name == CompleteProfileScreen.name) {
+          widget = const CompleteProfileScreen();
+        } else if (settings.name == HomeScreen.name) {
+          widget = const HomeScreen();
+        } else if (settings.name == MainBottomNavScreen.name) {
+          widget = const MainBottomNavScreen();
+        } else if (settings.name == CategoryListScreen.name) {
+          widget = const CategoryListScreen();
+        } else if (settings.name == ProductListScreen.name) {
+          String name = settings.arguments as String;
+          widget = ProductListScreen(categoryName: name);
+        }
+        return MaterialPageRoute(builder: (ctx) {
+          return widget;
+        });
       },
     );
   }
